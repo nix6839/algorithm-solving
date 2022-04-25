bd1c5dbf916704ff7615435bbcc3f7cd2b766747
Nix <nix6839@washnix.com>
2022-04-25 22:12:58 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,5 @@
+const readFileSync = require('node:fs').readFileSync;
+
+const input = readFileSync('/dev/stdin', 'utf-8');
+const [a, b] = input.split(' ').map((str) => parseInt(str, 10));
+console.log(a - b);
