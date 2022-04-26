efb979790b8e89ed31bdc625ce2ee83ece839c66
Nix <nix6839@washnix.com>
2022-04-26 12:39:20 +0900

    [Bronze I] Title: 더하기 사이클, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.js
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.js
@@ -2,19 +2,17 @@ const { readFileSync } = require('node:fs');
 
 const input = readFileSync('/dev/stdin', 'utf-8');
 
-const TWO_DIGIT_N = input.trim().padStart(2, '0');
+const N = parseInt(input, 10);
 
 function getCycle(originN) {
   let currentN = originN;
   for (let cycle = 1; ; cycle += 1) {
-    const sum = parseInt(currentN[0], 10) + parseInt(currentN[1], 10);
-    const twoDigitSum = sum.toString().padStart(2, '0');
-    currentN = currentN[1] + twoDigitSum[1];
-
-    if (originN === currentN) {
+    const sum = Math.floor(currentN / 10) + (currentN % 10);
+    currentN = (currentN % 10) * 10 + (sum % 10);
+    if (currentN === originN) {
       return cycle;
     }
   }
 }
 
-console.log(getCycle(TWO_DIGIT_N));
+console.log(getCycle(N));
