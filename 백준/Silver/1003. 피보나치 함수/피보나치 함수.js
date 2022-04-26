32f715960ca6b30571140f7f040894bd7923ef9b
Nix <nix6839@washnix.com>
2022-04-26 22:59:00 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 9624 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
@@ -6,19 +6,19 @@ const [T, ...STR_N_LIST] = input.trim().split('\n');
 
 const N_LIST = STR_N_LIST.map((str) => parseInt(str, 10));
 
-const memo = new Map([
-  [0, [1, 0]],
-  [1, [0, 1]],
-]);
+const memo = {
+  0: [1, 0],
+  1: [0, 1],
+};
 
 for (let i = 2; i <= 40; i += 1) {
-  const [zeroCount1, oneCount1] = memo.get(i - 1);
-  const [zeroCount2, oneCount2] = memo.get(i - 2);
-  memo.set(i, [zeroCount1 + zeroCount2, oneCount1 + oneCount2]);
+  const [zeroCount1, oneCount1] = memo[i - 1];
+  const [zeroCount2, oneCount2] = memo[i - 2];
+  memo[i] = [zeroCount1 + zeroCount2, oneCount1 + oneCount2];
 }
 
 let answer = '';
 N_LIST.forEach((N) => {
-  answer += `${memo.get(N).join(' ')}\n`;
+  answer += `${memo[N].join(' ')}\n`;
 });
 console.log(answer);
