ceb59e375082e25a1db2d1ed2809c560be809509
Nix <nix6839@washnix.com>
2022-04-26 11:50:00 +0900

    [Bronze III] Title: A+B - 5, Time: 132 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -3,11 +3,9 @@ const { readFileSync } = require('node:fs');
 const input = readFileSync('/dev/stdin', 'utf-8');
 
 const A_AND_B_STR_LIST = input.split('\n');
-const A_AND_B_LIST = A_AND_B_STR_LIST.map((A_AND_B_STR) =>
-  A_AND_B_STR.split(' ').map((str) => parseInt(str, 10)),
-);
 
-for (const [A, B] of A_AND_B_LIST) {
+for (const A_AND_B_STR of A_AND_B_STR_LIST) {
+  const [A, B] = A_AND_B_STR.split(' ').map((str) => parseInt(str, 10));
   if (A === 0 && B === 0) {
     break;
   }
