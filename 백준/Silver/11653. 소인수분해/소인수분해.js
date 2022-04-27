3e317c582be143f6b2af9a71bcfc6433e492813d
Nix <nix6839@washnix.com>
2022-04-28 08:21:04 +0900

    [Silver V] Title: 소인수분해, Time: 200 ms, Memory: 9620 KB -BaekjoonHub

--- 백준/Silver/11653. 소인수분해/소인수분해.js
+++ 백준/Silver/11653. 소인수분해/소인수분해.js
@@ -4,42 +4,12 @@ const input = readFileSync('/dev/stdin', 'utf-8');
 
 let n = parseInt(input.trim(), 10);
 
-function isPrime(number) {
-  if (number <= 1) {
-    return false;
-  }
-  if (number === 2) {
-    return true;
-  }
-  if (number % 2 === 0) {
-    return false;
-  }
-  const MAX = Math.floor(Math.sqrt(number));
-  for (let i = 3; i <= MAX; i += 2) {
-    if (number % i === 0) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function* primeGenerator() {
-  yield 2;
-  for (let i = 3; ; i += 2) {
-    if (isPrime(i)) {
-      yield i;
-    }
+for (let i = 2; i * i <= n; i += 1) {
+  while (n % i === 0) {
+    console.log(i);
+    n /= i;
   }
 }
-
-const primeGenerate = primeGenerator();
-
-let prime = primeGenerate.next().value;
-while (n > 1) {
-  if (n % prime === 0) {
-    n /= prime;
-    console.log(prime);
-    continue;
-  }
-  prime = primeGenerate.next().value;
+if (n !== 1) {
+  console.log(n);
 }
