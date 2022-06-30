3a9874648e00145ab837f99f8d9dc424101d385b
Nix <nix6839@washnix.com>
2022-06-30 21:16:05 +0900

    compl: law_of_large_numbers

--- 이취코 with 파이썬/a_greedy/a_greedy/law_of_large_numbers.py
+++ 이취코 with 파이썬/a_greedy/a_greedy/law_of_large_numbers.py
@@ -12,15 +12,20 @@ def law_of_large_numbers(M: int, K: int, nums: List[int]) -> int:
     K (1 <= K <= 10,000): 똑같은 인덱스가 연속으로 K번까지만 더해질 수 있다.
     nums: 숫자들이 담겨있는 배열 (2 <= len(nums) <= 1000)
     '''
-    max_num, second_max_num, *_ = sorted(nums, reverse=True)
-    print(max_num, second_max_num)
-    return 46
+    max_num, second_num, *_ = sorted(nums, reverse=True)
+
+    second_num_count = M // K
+
+    total_max_num = (M - second_num_count) * max_num
+    total_second_num = second_num_count * second_num
+
+    return total_max_num + total_second_num
 
 
 if __name__ == '__main__':
     def main():
         _N, M, K = map(int, input().split())
         nums = list(map(int, input().split()))
-        law_of_large_numbers(M, K, nums)
+        print(law_of_large_numbers(M, K, nums))
 
     main()
