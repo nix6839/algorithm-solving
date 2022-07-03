2b5c6286cab383c4906cdbfce89df6e39690c9b0
Han Yeong-woo <nix6839@washnix.com>
2022-07-03 13:52:26 +0900

    refactor: f_reverse_string

--- 이취코 with 파이썬/a_greedy/a_greedy/f_reverse_string.py
+++ 이취코 with 파이썬/a_greedy/a_greedy/f_reverse_string.py
@@ -6,17 +6,17 @@
 def f_reverse_string(binary_str: str) -> int:
     zero_count = 0
     one_count = 0
-    prev_is_zero = False
-    prev_is_one = False
+    prev_binary_char = None
+
     for binary_char in binary_str:
+        prev_is_zero = prev_binary_char == '0'
+        prev_is_one = prev_binary_char == '1'
+
         if binary_char == '0' and not prev_is_zero:
             zero_count += 1
-            prev_is_zero = True
-            prev_is_one = False
         elif binary_char == '1' and not prev_is_one:
             one_count += 1
-            prev_is_one = True
-            prev_is_zero = False
+        prev_binary_char = binary_char
 
     return min(zero_count, one_count)
 
