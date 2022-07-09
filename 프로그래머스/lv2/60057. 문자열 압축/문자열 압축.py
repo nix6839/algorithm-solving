01d0f9a6c3cf397ddd60dd1ed43d3f19d2d94958
Han Yeong-woo <nix6839@washnix.com>
2022-07-09 19:26:48 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -2,23 +2,22 @@ from functools import reduce
 
 
 def compressed_len(to_compress: str, unit_len: int) -> int:
-    unit = ''
+    unit = to_compress[:unit_len]
     count = 1
     ret = 0
 
-    for i in range(0, len(to_compress), unit_len):
-        temp = to_compress[i:i + unit_len]
-        if unit == temp:
-            count += 1
-        elif unit != temp:
-            ret += len(temp)
+    i = unit_len
+    while len(unit) != 0:
+        next_unit = to_compress[i:i + unit_len]
+        if unit != next_unit:
+            ret += len(unit)
             if count > 1:
                 ret += len(str(count))
                 count = 1
-            unit = temp
-
-    if count > 1:
-        ret += len(str(count))
+            unit = next_unit
+        else:
+            count += 1
+        i += unit_len
 
     return ret
 
