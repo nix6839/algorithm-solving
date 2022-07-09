6365b4c3f15531e1ef1796d2c94f0201a5304d91
Han Yeong-woo <nix6839@washnix.com>
2022-07-09 13:57:23 +0900

    refactor: rename invalid file

--- 이취코 with 파이썬/b_implementation/__init__.py
+++ 이취코 with 파이썬/b_implementation/__init__.py
@@ -1,2 +1,2 @@
 from .part3.a_lucky_straight import a_lucky_straight as part3_a_lucky_straight
-from .part3.b_resort_straight import b_resort_string as part3_b_resort_string
+from .part3.b_resort_string import b_resort_string as part3_b_resort_string
