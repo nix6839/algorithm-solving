19043f722ccff11c88fffd5a04d307547ef7b281
Han Yeong-woo <nix6839@washnix.com>
2022-07-09 18:25:18 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/README.md
+++ 프로그래머스/lv2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=python3) 
 
 ### 성능 요약
 
-메모리: 29.9 MB, 시간: 0.13 ms
+메모리: 10.4 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,3 +1,30 @@
-def solution(s):
-    answer = 0
-    return answer
+from functools import reduce
+
+
+def compressed_len(to_compress: str, unit_len: int) -> int:
+    unit = to_compress[:unit_len]
+    count = 1
+    ret = 0
+
+    i = unit_len
+    while len(unit) != 0:
+        next_unit = to_compress[i:i + unit_len]
+        if unit != next_unit:
+            ret += len(unit)
+            if count > 1:
+                ret += len(str(count))
+                count = 1
+            unit = next_unit
+        else:
+            count += 1
+        i += unit_len
+
+    return ret
+
+
+def solution(string: str) -> int:
+    return reduce(
+        lambda min_len, unit: min(min_len, compressed_len(string, unit)),
+        range(1, 1 + (len(string) // 2)),
+        len(string)
+    )
