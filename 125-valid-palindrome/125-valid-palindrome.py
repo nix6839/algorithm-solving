a3e07338f9c9514dced544f77ab1c6737b3f947a
Han Yeong-woo <nix6839@washnix.com>
2022-09-17 08:27:28 +0900

    Time: 71 ms (62.36%), Space: 15.5 MB (17.27%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -2,15 +2,7 @@ class Solution:
     @staticmethod
     def remove_non_alphanumeric(s: str) -> str:
       return re.sub(r'[^a-zA-Z0-9]', '', s)
-    
-    @staticmethod
-    def cut_half_str(s: str) -> tuple[str, str]:
-      half_len = len(s) // 2
-      if (len(s) % 2 == 0):
-        return (s[:half_len], s[half_len:])
-      return (s[:half_len], s[half_len + 1:])
 
     def isPalindrome(self, s: str) -> bool:
         s = Solution.remove_non_alphanumeric(s).lower()
-        front, rear = Solution.cut_half_str(s)
-        return front == rear[::-1]
+        return s == s[::-1]
