36f87067b620789d0a10d0e61f6da6e7b9e43c13
Han Yeong-woo <nix6839@washnix.com>
2022-09-17 10:42:22 +0900

    Time: 64 ms (41.20%), Space: 14 MB (38.07%) - LeetHub

--- 819-most-common-word/819-most-common-word.py
+++ 819-most-common-word/819-most-common-word.py
@@ -2,12 +2,12 @@ from collections import Counter
 
 class Solution:
     @staticmethod
-    def remove_non_samllnumericspace(s: str) -> str:
+    def non_smallnumeric_to_space(s: str) -> str:
       return re.sub(r'[^a-z0-9 ]', ' ', s)
 
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         most_commons = Counter(
-          Solution.remove_non_samllnumericspace(paragraph.lower()).split()
+          Solution.non_smallnumeric_to_space(paragraph.lower()).split()
         ).most_common()
         for word, _ in most_commons:
           if word not in banned:
